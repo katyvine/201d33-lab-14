a9a91af3d090ee196ff6e86301b81378addf4c0d
EmeryP <emeryparks09@hotmail.com>
2018-03-26 22:23:27 -0700

    worked on adding counter to cart page

--- lab/js/cart.js
+++ lab/js/cart.js
@@ -2,10 +2,13 @@
 
 var Cart = [];
 
+// var countElement = document.getElementById('itemCount');
+
+// countElement.textContent = counter;
+
 
 function loadCart() {
   // TODO: Pull the cart (if it exists) from Local Storage and make available to this app
-
   var cartAsString = localStorage.getItem('storedCart');
   var usableCart = JSON.parse(cartAsString);
   if (usableCart && usableCart.length) {
@@ -14,39 +17,46 @@ function loadCart() {
     return;
 
   }
+
+  // var cartCountAsString = localStorage.getItem('savedCart');
+  // var usableCartCount = JSON.parse(cartCountAsString);
+  // if (usableCartCount && usableCartCount.length) {
+  //   counter = usableCartCount;
+  //   console.log ('loaded from local storage');
+  //   return;
+  // }
 }
 
-
-// TODO: Fill in the <tr>'s under the <tbody> for each item in the cart  
-
+// TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
 
   // TODO: Find the table
-  var cartElement = document.getElementById("cart");
+  var cartElement = document.getElementById('cart');
 
   // TODO: Iterate over the items in the cart
   for (var i=0; i < Cart.cartItems.length; i++) {
 
-  // TODO: Create a TR
-  var trElement = document.createElement('tr');
-  // TODO: Create a TD for the quantity and the item
-  var tdElement = document.createElement('td');
+    // TODO: Create a TR
+    var trElement = document.createElement('tr');
+    // TODO: Create a TD for the quantity and the item
+    var tdElement = document.createElement('td');
 
-  tdElement.textContent = 'X';
-  trElement.appendChild(tdElement);
-  tdElement = document.createElement('td');
+    tdElement.textContent = 'X';
+    trElement.appendChild(tdElement);
+    tdElement = document.createElement('td');
 
-  // TODO: Add the TR to the TBODY and both TD's to the TR
-  tdElement.textContent = Cart.cartItems[i].item;
-  trElement.appendChild(tdElement);
+    // TODO: Add the TR to the TBODY and both TD's to the TR
+    tdElement.textContent = Cart.cartItems[i].item;
+    trElement.appendChild(tdElement);
 
-  tdElement = document.createElement('td');
+    tdElement = document.createElement('td');
 
-  tdElement.textContent = Cart.cartItems[i].quantity;
-  trElement.appendChild(tdElement);
+    tdElement.textContent = Cart.cartItems[i].quantity;
+    trElement.appendChild(tdElement);
 
-  cartElement.appendChild(trElement);
+    cartElement.appendChild(trElement);
   }
+
 }
 
 
--- lab/js/catalog.js
+++ lab/js/catalog.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var counter = 1;
+var counter = 0;
 
 // Access element from DOM
 var itemElement = document.getElementById('items');
@@ -9,7 +9,6 @@ var countElement = document.getElementById('itemCount');
 
 function populateForm() {
   //TODO: Add an <option> tag inside the form's select for each product
- 
   for (var i in Product.allProducts) {
     var itemElementTag = document.createElement('option');
     itemElementTag.textContent = Product.allProducts[i].name;
@@ -28,8 +27,6 @@ function handleSubmit(event) {
   updateCartPreview();
 }
 
-
-
 function addSelectedItemToCart() {
   // TODO: Add the selected item and quantity to the cart
   var newItem = event.target.items.value;
@@ -38,23 +35,24 @@ function addSelectedItemToCart() {
   new Cart (newItem, newQuantity);
 }
 
-
 function saveCartToLocalStorage() {
   // TODO: Save the cart to Local Storage
   var saveCart = JSON.stringify(Cart.cartItems);
   localStorage.setItem('storedCart', saveCart);
 }
 
-
-
 function updateCounter() {
   // TODO: Update the cart count in the header
   countElement.textContent = counter++;
   console.log(counter);
+
+  var saveCount = JSON.stringify(counter);
+  localStorage.setItem('savedCount', saveCount);
 }
 
 function updateCartPreview() {
   // TODO: Show the selected item & quantity in the cart div
+
 }
 
 // TODO: Put an event listener on the #catalog so that you can run the "handleSubmit" method when user submits the form (adding an item to their cart)
